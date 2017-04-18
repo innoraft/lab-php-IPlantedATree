cd1d92da6053d497f57bc3678024aa4ffe22e6d5
vivekpan27 <vivek.panicker@innoraft.com>
2017-04-18 15:55:59 +0530

    Implemented posting in wall, tagging with friends.

---
+++ login2_1.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+require_once __DIR__ . '/vendor/autoload.php';
+$fb = new Facebook\Facebook([
+			'app_id' => '1867029653544963',
+			'app_secret' => 'ab7e90234d0bb4fbb27d160fb93a4479',
+			'default-graph_version' => 'v2.5'
+		]);
+
+$helper = $fb->getRedirectLoginHelper();
+
+$permissions = ['email','publish_actions','user_about_me','user_friends','user_posts']; // Optional permissions
+$loginUrl = $helper->getLoginUrl('http://treeplant123.com/callback2.php', $permissions);
+
+echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
+?>
